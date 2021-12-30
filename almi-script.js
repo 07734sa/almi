9f17d3c80155a3a6a77d4768c0c21fc9802f75be
50773sa <93046590+50773sa@users.noreply.github.com>
2021-12-30 18:41:46 +0100

    Revert "Simplified event listener with only if"
    This reverts commit a87e3517b19460953cbb734516d25851c2fdd3d4.

--- almi-script.js
+++ almi-script.js
@@ -1,5 +1,6 @@
 const mainNavEl = document.querySelectorAll('.mainNav');
 
+const mainNavDropEl = document.querySelector('.mainNavDrop');
 const aOneEl= document.querySelector('.aOne');
 const aOneWrapEl = document.querySelector('.aOneWrap');
 
@@ -12,13 +13,14 @@ const aThreeWrapEl = document.querySelector('.aThreeWrap');
 const searchBtnEl = document.querySelector('.searchBtn')
 const searchBarEl = document.querySelector('.searchBar')
 
-const toggleButtonEl = document.querySelector('.toggleButton');
-const burgerLinksWrapEl = document.querySelector('.burgerLinksWrap');
+const toggleButton = document.querySelector('.toggleButton');
+const burgerLinksWrap = document.querySelector('.burgerLinksWrap');
 
 
 //mobile navigation
-toggleButtonEl.addEventListener('click', () => {
-    burgerLinksWrapEl.classList.toggle('showLinks')
+toggleButton.addEventListener('click', () => {
+    //console.log(burgerLinksWrap.classList)
+    burgerLinksWrap.classList.toggle('showLinks')
 });
 
 //search bar
@@ -33,15 +35,24 @@ mainNavEl.forEach(link => {
 
         if (e.target === aOneEl) {
             aOneWrapEl.classList.toggle('show'); 
-        }   
+
+        }   else {
+            aOneWrapEl.classList.remove('show');
+        }
 
         if (e.target === aTwoEl) {
             aTwoWrapEl.classList.toggle('show');
-        } 
+
+        }   else {
+            aTwoWrapEl.classList.remove('show');
+        }
         
         if (e.target === aThreeEl) {
             aThreeWrapEl.classList.toggle('show');
-        }   
+
+        }   else {
+            aThreeWrapEl.classList.remove('show');
+        }  
     });
 });   
    
